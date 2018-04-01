d911b48f64134fe4a885ce02ae340521f1450cb2
MW_01 <marco.wyssmann@students.ffhs.ch>
2018-04-01 21:43:11 +0200

    zwischenstand

--- src/main/java/divisor/CalculateDivisor.java
+++ src/main/java/divisor/CalculateDivisor.java
@@ -1,15 +1,19 @@
 package divisor;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.apache.commons.collections.list.SynchronizedList;
+
 /**
  * Das folgende Programm soll aus einem vorgegebene Interval von Long-Zahlen die
- * Zahl zurückgeben, die die meisten Divisoren hat. Sollte es mehrere solche
- * Zahlen geben, so soll die kleinste dieser Zahlen ausgegeben werden.
+ * Promzahlen zurückgeben.
  * 
  * Die Berechnung soll in n Threads stattfinden, die via Executor Framework
  * gesteuert werden, und sich das Problem aufteilen - jeder Thread soll eine
@@ -21,52 +25,104 @@ import java.util.concurrent.Future;
  */
 public class CalculateDivisor {
 
-	long von, bis;
-	int threadCount;
+    private long von;
+    private long bis;
+    private int threads;
+    private Collection<Callable<DivisorResult>> tasks = new ArrayList<>();
+    final ExecutorService executorService = Executors.newFixedThreadPool(threads);
 
-	/**
-	 * @param von
-	 *            untere Intervallgrenze
-	 * @param bis
-	 *            obere Intervallgrenze
-	 * @param threadCount
-	 *            Abzahl der Threads, auf die das Problem aufgeteilt werden soll
-	 */
-	public CalculateDivisor(long von, long bis, int threadCount) {
-		this.von = von;
-		this.bis = bis;
-		this.threadCount = threadCount;
+    /**
+     * @param von
+     *            untere Intervallgrenze
+     * @param bis
+     *            obere Intervallgrenze
+     * @param threadCount
+     *            Abzahl der Threads, auf die das Problem aufgeteilt werden soll
+     */
+    public CalculateDivisor(long von, long bis, int threads) {
+	this.von = von;
+	this.bis = bis;
+	this.threads = threads;
+	tasksInitialisieren();
 
+    }
+
+    private void tasksInitialisieren() {
+	
+	long grösseZahlenfolge = bis - von;
+	long grösseTeilZahlenfolge = grösseZahlenfolge / threads;
+	long vonTeil = von;
+	long bisTeil = vonTeil + grösseZahlenfolge;
+	
+	for (int i = 0; i < threads; i++) {
+	    
+	    vonTeil = 0;
+	    bisTeil = 0;
+	    tasks.add(new PrimzahlTask(von, bis));
 	}
 
-	/**
-	 * Berechnungsroutine
-	 * 
-	 * @return
-	 * @throws InterruptedException
-	 * @throws ExecutionException
-	 */
-	DivisorResult calculate() throws InterruptedException, ExecutionException {
+    }
 
-		// TODO implementieren Sie hier die Logic der calculate Methode
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+	if (args.length != 3) {
+	    System.out.println("Usage: CalculateDivisor <intervalStart> <intervalEnd> <threadCount>");
+	    System.exit(1);
+	}
+	long von = Long.parseLong(args[0]);
+	long bis = Long.parseLong(args[1]);
+	int threads = Integer.parseInt(args[2]);
 
-		return new DivisorResult(0, 0);
+	CalculateDivisor cd = new CalculateDivisor(von, bis, threads);
+	// System.out.println("Ergebnis: " + cp.calculate());
+    }
+
+    /**
+     * Berechnungsroutine
+     * 
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    DivisorResult calculate() throws InterruptedException, ExecutionException {
+
+	// Wie bei accessResults S.609 Future gibt WLösung zurück
+	// TODO implementieren Sie hier die Logic der calculate Methode
+
+	return new DivisorResult();
+    }
+
+    public final class PrimzahlTask implements Callable<DivisorResult> {
+
+	private long von;
+	private long bis;
+	private DivisorResult resultat = new DivisorResult();
+
+	public PrimzahlTask(long von, long bis) {
+
+	    this.von = von;
+	    this.bis = bis;
 	}
 
-	public static void main(String[] args) throws InterruptedException,
-			ExecutionException {
-		if (args.length != 3) {
-			System.out
-					.println("Usage: CalculateDivisor <intervalStart> <intervalEnd> <threadCount>");
-			System.exit(1);
+	public DivisorResult call() {
+	    int anzTeiler;
+	    for (long zahl = von; zahl <= bis; zahl++) {
+		anzTeiler = 0;
+		for (long divisor = 1; divisor <= zahl; divisor++) {
+		    if (zahl % divisor == 0) {
+			anzTeiler++;
+		    }
+		    if (anzTeiler > 2) {
+			break;
+		    }
 		}
-		long von = Long.parseLong(args[0]);
-		long bis = Long.parseLong(args[1]);
-		int threads = Integer.parseInt(args[2]);
+		if (anzTeiler == 2) {
+		    resultat.primzahlHinzufügen(zahl);
+		}
+	    }
 
-		CalculateDivisor cp = new CalculateDivisor(von, bis, threads);
-		System.out.println("Ergebnis: " + cp.calculate());
+	    return resultat;
 	}
+    }
 
 }
 
@@ -78,29 +134,31 @@ public class CalculateDivisor {
  * 
  */
 class DivisorResult {
-	// das eigentlich ergebnis - die Zahl mit der max. Anzahl von Divisoren
-	long result;
+    // das eigentlich ergebnis - die Ermittelten Primzahlen
+    private final List<Long> primzahlenListe = new ArrayList();
 
-	// Anzahl der Divisoren von Result
-	long countDiv;
+    // Anzahl der Divisoren von Result
 
-	public DivisorResult(long r, long c) {
-		result = r;
-		countDiv = c;
+    public DivisorResult() {
+
+    }
+
+    public List<Long> getPrimzahlen() {
+
+	return primzahlenListe;
+
+    }
+
+    public void primzahlHinzufügen(long primzahl) {
+
+	synchronized (primzahlenListe) {
+	    primzahlenListe.add(primzahl);
 	}
+    }
 
-	public long getResult() {
-		return result;
-	}
-
-	public long getCountDiv() {
-		return countDiv;
-	}
-
-	@Override
-	public String toString() {
-		return "Zahl mit maximaler Anzahl Divisoren: " + result + " ("
-				+ countDiv + " Divisoren)";
-	}
+    @Override
+    public String toString() {
+	return "Die Primzahlen lauten: " + primzahlenListe;
+    }
 
 }
